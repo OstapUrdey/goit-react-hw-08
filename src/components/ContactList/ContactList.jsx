45ddaa389ed7e235a6bd37bf28842764831c6373
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-09-23 19:12:40 +0300

    fixed components ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,17 +4,11 @@ import Contact from '../Contact/Contact.jsx'
 import {useSelector, useDispatch} from 'react-redux';
 import {selectFilteredContacts} from '../../redux/contactsSlice.js';
 import { deleteContact } from '../../redux/contactsOps.js';
-import {selectNameFilter} from '../../redux/filtersSlice.js';
 
 export default function ContactList() {
 
     const contacts = useSelector(selectFilteredContacts);
-    const search = useSelector(selectNameFilter);
     const dispatch = useDispatch();
-
-    const filteredContacts = contacts.filter(contact =>
-        contact.name.toLowerCase().includes(search.toLowerCase())
-    );
 
     const handleDeleteContact = (id) => {
         dispatch(deleteContact(id));
@@ -22,7 +16,7 @@ export default function ContactList() {
 
     return (
         <ul className={css.contactList}>
-            {filteredContacts.map(contact => (
+            {contacts.map(contact => (
                 <Contact key={contact.id} {...contact} deleteContact={handleDeleteContact} />
             ))}
         </ul>
