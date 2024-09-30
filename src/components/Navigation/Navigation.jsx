dd7de3074bec08b9c898d4c836ceed80da08a9fd
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-09-30 15:18:15 +0300

    str3

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,23 @@
 import css from './Navigation.module.css';
-import { NavLink } from 'react-router-dom'; 
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
     return (
-        <nav className={css.nav}>
-            <NavLink className={css.link} to="/">
-                Home
-            </NavLink>
-            <NavLink className={css.link} to="/contacts">
-                Contacts
-            </NavLink>
-            <NavLink className={css.link} to="/login">
-                Login
-            </NavLink>
-            <NavLink className={css.link} to="/register">
-                Register
-            </NavLink>
+        <nav className={css.navbar}>
+            <ul className={css.navList}>
+                <li className={css.navItem}>
+                    <NavLink className={css.navLink} to="/">Home</NavLink>
+                </li>
+                <li className={css.navItem}>
+                    <NavLink className={css.navLink} to="/contacts">Contacts</NavLink>
+                </li>
+                <li className={css.navItem}>
+                    <NavLink className={css.navLink} to="/login">Login</NavLink>
+                </li>
+                <li className={css.navItem}>
+                    <NavLink className={css.navLink} to="/register">Register</NavLink>
+                </li>
+            </ul>
         </nav>
-    )
+    );
 }
